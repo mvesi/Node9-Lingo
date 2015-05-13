752f5034c9459dd133c957deb99ab7b4556de2cd
Michael Genovesi <michaelgenovesi@gmail.com>
2015-05-13 13:04:19 -0600

    lunch break

--- controllers/index.js
+++ controllers/index.js
@@ -1,24 +1,35 @@
 var BeGlobal = require('node-beglobal');
+var beglobal = new BeGlobal.BeglobalAPI({
+  api_token: '7%2F8kcSz7bSXtoTRMaeY8ow%3D%3D'
+});
 
+var languages = [
+    ['English', 'eng'],
+    ['French', 'fra'],
+    ['Spanish','spa'],
+    ['German', 'ger'],
+    ['Portuguese', 'por'],
+    ['Albanian', 'alb'],
+    ['Italian', 'ita'],
+    ['Swedish', 'swe'],
+    ['Slovak', 'slo']
+];
 
 
 var indexController = {
 	index: function(req, res) {
-		res.render('index');
+		res.render('index', {
+            languages: languages
+        });
 	},
     translator: function(req, res){
-        res.send(req.body);
-        // console.log(req.body);
-        var beglobal = new BeGlobal.BeglobalAPI({
-          api_token: '7%2F8kcSz7bSXtoTRMaeY8ow%3D%3D'
-        });
 
         beglobal.translations.translate(
-          {text: 'hello', from: 'eng', to: 'fra'},
-          function(err, results) {
-            if (err) {
-              return console.log(err);
-            }
+            {text: req.body.text, from: req.body.from, to: req.body.to},
+                function(err, results) {
+                if (err) {
+                  return console.log(err);
+                }
 
             res.send(results);
           }
--- public/main.js
+++ public/main.js
@@ -1,13 +1,27 @@
+
+
 $(document).on('ready', function(){
 
     $('#translator').on('submit', function(e){
         e.preventDefault();
-        var fromLanguage = $(this).find('[name="language-from"]').val();
-        var toLanguage = $(this).find('[name="language-to"]').val();
-        var word = $(this).find('[name="word"]').val();
-        var dataArr = [fromLanguage, toLanguage, word];
-        console.log(fromLanguage);
-        $.post('/translator', dataArr, function(data){
+        var from = $(this).find('[name="from"]').val();
+        var to = $(this).find('[name="to"]').val();
+        var text = $(this).find('[name="text"]').val();
+        // var dataArr = [fromLanguage, toLanguage, word];
+        
+        var transData = {text: text};
+
+        for(var i=0 ; i<languages.length ; i++){
+            if(from === languages[i][0]){
+                transData['from'] = languages[i][1];
+            }
+             if(to === languages[i][0]){
+                transData['to'] = languages[i][1];
+            }
+        }
+
+        console.log(transData);
+        $.post('/translator', transData, function(data){
             console.log('Server says:', data);
         });
 
